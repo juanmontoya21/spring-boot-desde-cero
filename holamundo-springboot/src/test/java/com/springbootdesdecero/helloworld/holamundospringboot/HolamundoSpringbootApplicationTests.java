8475233b7b8b4f8bf1b94c6421c159f0ec4b04a4
Juan Montoya <juanmontoya18>
2023-10-20 17:23:13 -0500

    se crea un proyecto hola mundo con un archivo controller

---
+++ holamundo-springboot/src/test/java/com/springbootdesdecero/helloworld/holamundospringboot/HolamundoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdesdecero.helloworld.holamundospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
