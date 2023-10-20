f19368f31c7b55d6f48f8a875cf7b075c3c0cd46
Juan Montoya <juanmontoya18>
2023-10-20 17:21:33 -0500

    este el archivo por defecto para desplegar nuestro proyecto

---
+++ holamundo-springboot/src/main/java/com/springbootdesdecero/helloworld/holamundospringboot/HolamundoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdesdecero.helloworld.holamundospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoSpringbootApplication.class, args);
+	}
+
+}
